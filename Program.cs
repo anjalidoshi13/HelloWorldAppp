fb9aa78f815897a9bdba050f1a92a7fd65c6ff93
Anjali Doshi <anjalikothari05222@gmail.com>
2023-05-08 15:32:11 +0530

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HelloWorldAppp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
